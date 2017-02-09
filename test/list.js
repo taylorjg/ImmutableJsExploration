7463d36d4ca89395ee911cffea8813098ba9b735
Jon Taylor <jont@cakesolutions.net>
2017-02-09 16:57:14 +0000

    Tweaks

--- test/list.js
+++ test/list.js
@@ -1,11 +1,14 @@
 const Immutable = require('immutable');
 const chai = require('chai');
 const chaiImmutable = require('chai-immutable');
+
 const assert = chai.assert;
+const List = Immutable.List;
+
 chai.use(chaiImmutable);
 
 describe('List', () => {
     it('first basic test', function () {
-        assert.equal(Immutable.List.of(1, 2, 3), Immutable.List.of(1, 2, 3));
+        assert.equal(List.of(1, 2, 3), List.of(1, 2, 3));
     });
 });
